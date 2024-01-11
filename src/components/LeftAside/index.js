e4995d6cf5f157c46bb91fdfc8676462d926d612
Pavel Chernov <pachernov@ozon.ru>
2024-01-11 23:02:00 +0300

    Initial commit

---
+++ src/components/LeftAside/index.js
@@ -0,0 +1,1 @@
+export { default as LeftAside } from "./LeftAside.hbs?raw";
---
+++ src/pages/index.ts
@@ -0,0 +1,8 @@
+export { MainPage } from "./Main";
+export { AuthPage } from "./Auth";
+export { RegistrationPage } from "./Registration";
+export { ErrorPage } from "./Error";
+export { ChatPage } from "./Chat";
+export { ProfilePage } from "./Profile";
+export { ProfileEditPage } from "./ProfileEdit";
+export { PasswordEditPage } from "./PasswordEdit";
